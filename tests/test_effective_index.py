1c8de30006a16b712fe68f329e46c57acf556160
Heath Smith <hsmith@thorlabs.com>
2022-05-06 09:57:13 -0400

    change sub_n_eff to effective_index update unit test

---
+++ tests/test_effective_index.py
@@ -0,0 +1,73 @@
+#!user/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+This module contains the test suite for tff_lib.effective_index().
+
+
+Examples
+---------
+>>> python -m unittest -v tests.test_tff_lib.test_effective_index
+"""
+
+# import external packages
+import numpy as np
+from numpy import testing as nptest
+import unittest
+from pathlib import Path
+import sys
+import os
+import json
+
+# import tff_lib for testing
+from tff_lib.tff_lib import effective_index
+
+class TestEffectiveIndex(unittest.TestCase):
+    """
+    Test class for effective_index()
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up the test environment.
+        """
+
+        # static navigation to data directory and output directory
+        cls.dir = os.path.join(Path(__file__).resolve().parent, r'data')
+        cls.out_dir = os.path.join(cls.dir, 'out')
+
+        # read in json file with test input data
+        with open(os.path.join(cls.dir, 'test_data.json')) as dat:
+            cls.test_data = json.load(dat)
+
+        # get test substrate array
+        cls.test_sub = np.asarray(cls.test_data['input']['substrate']).astype(np.complex)
+        cls.test_theta = 0
+        cls.test_exp = np.asarray(cls.test_data['output']['effective_index']).astype(np.complex)
+
+        # floating point comparison threshold
+        cls.precision = 14
+
+    def test_effective_index(self):
+        """
+        ----------> default test case
+        """
+
+        # test effective_index()
+        _eff = effective_index(self.test_sub, self.test_theta)
+
+        # verify the output
+        nptest.assert_array_almost_equal(self.test_exp, _eff, decimal=self.precision)
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Cleans up any open resources.
+        """
+
+        sys.stdout.write('Running teardown procedure...\nDONE!')
+        sys.stdout.close()
+
+if __name__=='__main__':
+    unittest.main()
--- tff_lib/tff_lib.py
+++ tff_lib/tff_lib.py
@@ -341,41 +341,44 @@ def fresnel_film(admittance:dict, char_matrix:dict) -> dict:
     return fresnel
 
 
-def sub_n_eff(s_n, theta, units='rad'):
+def effective_index(
+    sub:np.ndarray, theta:Union[int, float], units:str='rad') -> np.ndarray:
     """
     Calculates the effective substrate refractive index for abs. substrates.
 
     Parameters
     -----------
-    *args:\n
-    s_n (array):\n
-    theta (float):
-
-    **kwargs: (optional)\n
-    units (str):
+    sub = complex refractive index of substrate.\n
+    theta =
+    units =
 
     Returns
     ----------
-    (numpy.ndarray) Effective substrate refractive index.
+    (np.ndarray) Effective substrate refractive index.
     """
 
-    if units == 'deg':
-        # convert incident angle from degrees to radians
-        theta = theta * (np.pi / 180)
+    # check data types
+    if not isinstance(sub, np.ndarray):
+        raise TypeError(f'"sub" expects <np.ndarray>, received {type(sub)}.')
+    if not type(theta) in (int, float):
+        raise TypeError(f'"theta" expects <int> or <float>, received {type(theta)}.')
+    if not isinstance(units, str):
+        raise TypeError(f'"units" expects <str>, received {type(units)}.')
+    if units not in ('deg', 'rad'):
+        raise ValueError(f'"units" expects "deg" or "rad", received {units}.')
 
-    inside1 = (np.imag(s_n)**2 + np.real(s_n)**2)**2 + 2 * (np.imag(s_n) - \
-        np.real(s_n)) * (np.imag(s_n) + np.real(s_n)) * np.sin(theta)**2 + \
-        np.sin(theta)**4
-    inside1 = np.array(inside1, dtype=np.float64)
-    inside2 = 0.5 * (-np.imag(s_n)**2 + np.real(s_n)**2 + np.sin(theta)**2 + \
-        np.sqrt(inside1))
-    inside2 = np.array(inside2, dtype=np.float64)
-    sn_eff = np.sqrt(inside2)
+    # convert incident angle from degrees to radians
+    theta = theta * (np.pi / 180) if units == 'deg' else theta
 
-    return np.array(sn_eff)
+    # calculate the effective substrate refractive index
+    inside1 = ((np.imag(sub)**2 + np.real(sub)**2)**2 + 2 * (np.imag(sub) - np.real(sub))
+                * (np.imag(sub) + np.real(sub)) * np.sin(theta)**2 + np.sin(theta)**4)
+    inside2 = 0.5 * (-np.imag(sub)**2 + np.real(sub)**2 + np.sin(theta)**2 + np.sqrt(inside1))
+
+    return np.sqrt(inside2)
 
 
-def path_len(sub_thick, i_n, sn_eff, theta, units='rad'):
+def path_length(sub_thick, i_n, sn_eff, theta, units='rad'):
     """
     Calculates the optical path length through a given substrate.
 
