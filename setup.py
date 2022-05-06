b89788996e7696d7712fb87667f6c853b9497ba4
Heath Smith <hsmith@thorlabs.com>
2022-05-06 12:59:28 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='tff_lib',
-      version='1.2',
+      version='2.0.1',
       description='Thin Film Filter Library',
       url='https://ThorlabsSpectralWorks@dev.azure.com/ThorlabsSpectralWorks/tff_lib/_git/tff_lib',
       author='Ryan Priore, Heath Smith',
       author_email='rpriore@thorlabs.com, hsmith@thorlabs.com',
       license='MIT',
-      packages=['tff_lib'],
+      packages=['tff_lib', 'scripts', 'tests'],
       zip_safe=False)
