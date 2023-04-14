4220c4fea6356227b3975b5f2e04756d722569f1
Heath Smith <hsmith@thorlabs..com>
2023-04-14 10:02:04 -0400

    update __init__.py

--- tff_lib/__init__.py
+++ tff_lib/__init__.py
@@ -1,2 +1,2 @@
 from .tff_lib import fresnel_bare, admit_delta, char_matrix, fresnel_film, fil_spec
-import utils
+from .utils import film_matrix, plot_data, convert_to_numpy, effective_index, path_length
